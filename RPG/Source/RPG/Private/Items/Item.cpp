be6277da562b3ffcef84f41dd0e66788364f293b
hyunho-park777 <lumenize@ajou.ac.kr>
2026-02-17 23:13:01 +0900

    * [ Offset ]

--- RPG/Source/RPG/Private/Items/Item.cpp
+++ RPG/Source/RPG/Private/Items/Item.cpp
@@ -60,7 +60,14 @@ void AItem::Tick(float DeltaTime)
 		UE_LOG(LogTemp, Warning, TEXT("ItemName: %s"), *name);
 	}
 	*/
-	AddActorWorldOffset(FVector(1.f, 0.f, 0.f));
+
+	// Movement rate in units of cm/s
+	float MovementRate = 50.f;
+	float RotationRate = 45.f;
+	// MovementRate * DeltaTimes (cm/s) * (s/frame) = cm/frame
+	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
+	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
 	DRAW_SPHERE_SINGLEFRAME(GetActorLocation());
+	DRAW_VECTOR_SINGLEFRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 }
 
